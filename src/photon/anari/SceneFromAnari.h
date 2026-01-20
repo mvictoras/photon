99626e25157dd57810651d1da55b09cf63c6cbb3
Victor Mateevitsi <vmateevitsi@anl.gov>
2026-01-20 10:42:00 -0600

    chore: add scene-from-ANARI scaffold

---
+++ src/photon/anari/SceneFromAnari.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <anari/anari.h>
+
+#include <optional>
+
+#include "photon/pt/scene.h"
+
+namespace photon::anari_device {
+
+struct PhotonDevice;
+
+struct AnariSceneData
+{
+  photon::pt::Scene scene;
+};
+
+std::optional<AnariSceneData> build_scene_from_anari(ANARIWorld world, PhotonDevice &dev);
+
+}
